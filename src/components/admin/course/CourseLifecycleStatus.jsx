b716d65c0fdc37db3793d21d2d97906141cc6a75
tiktokyi <tiktokyi@gmail.com>
2025-01-25 19:24:27 +0900

    mod : NcsClassification의 영문을 한글로 변환

--- src/components/admin/course/CourseLifecycleStatus.jsx
+++ src/components/admin/course/CourseLifecycleStatus.jsx
@@ -36,7 +36,7 @@ const CourseLifecycleStatus = ({ courses }) => {
       field: "ncsClassification",
       headerName: "NCS 분류",
       valueGetter: (params) => {
-        return params.data.ncsClassification === "UNDEFINED" ? "미분류" : params.data.ncsClassification;
+        return ncsMapping[params.data.ncsClassification] || "미분류";
       },
       sortable: true,
       filter: 'agTextColumnFilter'
@@ -68,7 +68,7 @@ const CourseLifecycleStatus = ({ courses }) => {
     },
     {
       field: "setDuration",
-      headerName: "훈련 기간(일)",
+      headerName: "훈련 기간",
       valueGetter: (params) => `${params.data.setDuration}일`,
       sortable: true,
       filter: 'agNumberColumnFilter'
@@ -115,3 +115,31 @@ const CourseLifecycleStatus = ({ courses }) => {
 };
 
 export default CourseLifecycleStatus;
+
+const ncsMapping = {
+  UNDEFINED: "미분류",
+  BUSINESS_MANAGEMENT: "01. 사업관리",
+  MANAGEMENT_ACCOUNTING_OFFICE_WORK: "02. 경영·회계·사무",
+  FINANCE_INSURANCE: "03. 금융·보험",
+  EDUCATION_NATURAL_SOCIAL_SCIENCES: "04. 교육·자연·사회과학",
+  LAW_POLICE_FIRE_CORRECTIONS_NATIONAL_DEFENSE: "05. 법률·경찰·소방·교도·국방",
+  HEALTH_MEDICAL_CARE: "06. 보건·의료",
+  SOCIAL_WELFARE_RELIGION: "07. 사회복지·종교",
+  CULTURE_ARTS_DESIGN_BROADCASTING: "08. 문화·예술·디자인·방송",
+  DRIVING_TRANSPORTATION: "09. 운전·운송",
+  SALES_MARKETING: "10. 영업판매",
+  SECURITY_CLEANING: "11. 경비·청소",
+  ACCOMMODATION_TRAVEL_LEISURE_SPORTS: "12. 이용·숙박·여행·오락·스포츠",
+  FOOD_SERVICE: "13. 음식서비스",
+  CONSTRUCTION: "14. 건설",
+  MACHINERY: "15. 기계",
+  MATERIALS: "16. 재료",
+  CHEMISTRY_BIOTECHNOLOGY: "17. 화학·바이오",
+  TEXTILES_APPAREL: "18. 섬유·의복",
+  ELECTRICAL_ELECTRONICS: "19. 전기·전자",
+  INFORMATION_COMMUNICATIONS: "20. 정보통신",
+  FOOD_PROCESSING: "21. 식품가공",
+  PRINTING_WOODWORKING_FURNITURE_CRAFTS: "22. 인쇄·목재·가구·공예",
+  ENVIRONMENT_ENERGY_SAFETY: "23. 환경·에너지·안전",
+  AGRICULTURE_FORESTRY_FISHERIES: "24. 농림어업"
+};
